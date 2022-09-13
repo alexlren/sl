ef52385ce50084f00a0a82ce4c34c29da1b68e90
Alex Laurent <shaoner@pm.me>
2022-09-13 14:55:15 -0400

    Add support for options including -n N to select a specific animation

--- src/sl.c
+++ src/sl.c
@@ -4,8 +4,36 @@
 #include <assert.h>
 #include <time.h>
 #include <stdlib.h>
+#include <libgen.h>
+#include <errno.h>
 
 #include "generated/anims.h"
+
+#define OPTSTR "n:h"
+#define USAGE_FMT  "Usage: %s [-n N] [-h]\n"
+#define USAGE_DESC                                                      \
+    "A cruel program made to punish users who mispell ls\n\n"           \
+    "Available options:\n"                                              \
+    "\t-h\n"                                                            \
+    "\t\tDisplay this help\n"                                           \
+    "\t-n N\n"                                                          \
+    "\t\tSelect the Nth animation (instead of a random one)\n"
+
+
+extern int errno;
+extern char *optarg;
+extern int opterr, optind;
+
+struct sl_options {
+    /* Shows the nth animation */
+    int n;
+};
+
+static void usage(const char *progname) {
+    fprintf(stderr, USAGE_FMT, progname);
+    fprintf(stderr, USAGE_DESC);
+    exit(EXIT_FAILURE);
+}
 
 static char get_frame_char(struct animation *anim, int x, int y, int frame)
 {
@@ -77,8 +105,23 @@ static int display_animation(struct animation *anim, int x)
     return 0;
 }
 
-int main(void)
+static int start_animation(struct sl_options *options)
 {
+    struct animation *anim = NULL;
+
+    if (options->n < 0) {
+        /* Select a random animation */
+        srand(time(NULL));
+        int r = rand() % ANIMATION_COUNT;
+        anim = __animations_list[r];
+    } else {
+        if (options->n >= ANIMATION_COUNT) {
+            fprintf(stderr, "Error: invalid -n option, N should be in [0-%d]\n", ANIMATION_COUNT - 1);
+            return EXIT_FAILURE;
+        }
+        anim = __animations_list[options->n];
+    }
+
     initscr();
 #ifdef NDEBUG
     signal(SIGINT, SIG_IGN);
@@ -88,10 +131,6 @@ int main(void)
     nodelay(stdscr, TRUE);
     leaveok(stdscr, TRUE);
     scrollok(stdscr, FALSE);
-
-    srand(time(NULL));
-    int r = rand() % ANIMATION_COUNT;
-    struct animation *anim = __animations_list[r];
 
     for (int x = COLS - 1; ; --x) {
         if (display_animation(anim, x) != 0) {
@@ -105,4 +144,27 @@ int main(void)
     endwin();
 
     return 0;
+}
+
+int main(int argc, char **argv)
+{
+    int opt;
+    struct sl_options options = { .n = -1 };
+
+    opterr = 0;
+
+    while ((opt = getopt(argc, argv, OPTSTR)) != EOF) {
+        switch (opt) {
+        case 'n':
+            options.n = (int)strtoul(optarg, NULL, 10);
+            break;
+        case 'h':
+        default:
+            usage(basename(argv[0]));
+            /* exits here */
+            break;
+        }
+    }
+
+    return start_animation(&options);
 }
